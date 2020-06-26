461e546bfabf42df0969fb0c9b38c1b9ee81ceb2
Lunny Xiao <xiaolunwen@gmail.com>
2020-06-26 16:58:13 +0800

    Fi download bug

--- routers/repo/download.go
+++ routers/repo/download.go
@@ -22,7 +22,10 @@ import (
 // ServeData download file from io.Reader
 func ServeData(ctx *context.Context, name string, reader io.Reader) error {
 	buf := make([]byte, 1024)
-	n, _ := reader.Read(buf)
+	n, err := reader.Read(buf)
+	if err != nil && err != io.EOF {
+		return err
+	}
 	if n >= 0 {
 		buf = buf[:n]
 	}
@@ -46,7 +49,7 @@ func ServeData(ctx *context.Context, name string, reader io.Reader) error {
 		ctx.Resp.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, name))
 	}
 
-	_, err := ctx.Resp.Write(buf)
+	_, err = ctx.Resp.Write(buf)
 	if err != nil {
 		return err
 	}
